9d3ea297920b18a4d7921292409c166d3dae8709
superbabii <nzrbabii@gmail.com>
2024-10-16 15:14:52 +0300

    f

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import random
 from src.medrag import MedRAG
 
 # Load the benchmark JSON file
-with open('medmcqa.json', 'r') as f:
+with open('pubmedqa.json', 'r') as f:
     benchmark_data = json.load(f)
 
 # Get all questions
 all_questions = list(benchmark_data.items())
 
 # Limit to the first 200 questions
-# all_questions = all_questions[:800]
+all_questions = all_questions[:800]
 # all_questions = all_questions[600:1000]
 # all_questions = all_questions[400:600]
 # all_questions = all_questions[600:800]
 # all_questions = all_questions[800:1000]
 
 # Get random questions
-all_questions = random.sample(list(benchmark_data.items()), 100)
+# all_questions = random.sample(list(benchmark_data.items()), 100)
 
 # Initialize the MedRAG system
 cot = MedRAG(llm_name="axiong/PMC_LLaMA_13B", rag=False)
